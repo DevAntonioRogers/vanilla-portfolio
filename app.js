5ba990377eb93a4efc8c57779d796038e372fbf3
DevAntonioRogers <ar919061493@gmail.com>
2023-07-19 20:08:45 -0400

    Active Links

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // NAVBAR
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
+const body = document.querySelector('body')
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active')
   navMenu.classList.toggle('active')
+  body.classList.toggle('no-scroll')
+
 })
 
 // STICKY NAV
@@ -20,3 +23,22 @@ function handleScroll() {
   }
 }
 window.addEventListener('scroll', handleScroll);
+
+// Active Links
+
+
+const navLinks = document.querySelectorAll('.nav-menu li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    navLinks.forEach(link => link.parentElement.classList.remove('active'),
+      navMenu.classList.remove('active'),
+      hamburger.classList.remove('active'),
+      body.classList.remove('no-scroll'));
+    this.parentElement.classList.add('active');
+  });
+});
+
+
+
+
